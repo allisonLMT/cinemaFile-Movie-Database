d68ff1532ec81a836e2ed7202c3707cc55b01554
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-01-03 17:27:25 -0800

    added a css module for nav

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styles from './styles/nav.module.css';
 
 
 function Nav () {
     return (
         <nav>
             <ul>
-                <li><NavLink to="/" exact className="nav-link">Home</NavLink></li>
-                <li><NavLink to="/favorites" className="nav-link">Favorites</NavLink></li>
-                <li><NavLink to="/about" className="nav-link">About</NavLink></li> 
+                <li><NavLink to="/" exact className={styles.navLink}>Home</NavLink></li>
+                <li><NavLink to="/favorites" className={styles.navLink}>Favorites</NavLink></li>
+                <li><NavLink to="/about" className={styles.navLink}>About</NavLink></li> 
             </ul>
         </nav>
     );
