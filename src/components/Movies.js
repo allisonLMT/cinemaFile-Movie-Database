2edad9e2d97be30d8c3fe963a72189368088648c
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-01-12 15:48:40 -0800

    styling of movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from '../styles/movies.module.css';
 import RatingCircle from '../components/MovieRatingCircle.js';
-import LrgMoreInfo from '../components/LrgMoreInfo';
 import { BASE_URL, SM_POSTER_SIZE } from '../globals/variables.js';
 
 // movie poster, rating at top right
@@ -15,18 +14,21 @@ function Movies ({ moviesData }) {
         <div className={styles.movies}>
 
             {moviesData.map(movie => 
-                <div key={movie.id}>
+                <div className={styles.movie} key={movie.id}>
                 <img src={BASE_URL+SM_POSTER_SIZE+movie.poster_path} alt={movie.title} />
                 <h3>{movie.title}</h3>
-                <p>{movie.overview}</p>
-                <p>Release Date: {movie.release_date}</p>
+                
+                <p>{movie.release_date}</p>
+                
+                {/* <p>{movie.overview}</p> */}
                 </div>
-                )}
-
+                
+                )} 
+                {/* end of map */}
 
 
             <RatingCircle />
-            <LrgMoreInfo />
+            
             
             {/* <img src=`${BASE_URL}+"w500"+${API_KEY}` alt="" /> */}
             {/* <img src="https://api.themoviedb.org/3/configuration?api_key=API_KEY" alt="" /> */}
--- src/components/TopMovie.js
+++ src/components/TopMovie.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../styles/topMovie.module.css';
+import LrgMoreInfo from '../components/LrgMoreInfo';
 import { BASE_URL, SM_POSTER_SIZE } from '../globals/variables.js';
 
 
@@ -8,10 +9,11 @@ function TopMovie ({ topMovieData }) {
    
     return (
         <div className={styles.topMovie}>
-           <img src={BASE_URL+SM_POSTER_SIZE+topMovieData[0].poster_path} alt={topMovieData.title} />
-                <h3>{topMovieData.title}</h3>
-                <p>{topMovieData.overview}</p>
-                <p>Release Date: {topMovieData.release_date}</p> 
+            <img src={BASE_URL+SM_POSTER_SIZE+topMovieData[0].poster_path} alt={topMovieData.title} />
+            <LrgMoreInfo />
+            <h3>{topMovieData[0].title}</h3>
+            <p>Release Date: {topMovieData[0].release_date}</p> 
+            {/* <p>{topMovieData[0].overview}</p> */}
         </div>
     )
 
