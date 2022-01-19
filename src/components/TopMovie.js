ab6fcb0b51468fce18693c939c591cd97cc6ccba
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-01-18 16:19:15 -0800

    removed small more info

--- src/components/TopMovie.js
+++ src/components/TopMovie.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../styles/topMovie.module.css';
 import RatingCircle from '../components/MovieRatingCircle.js';
-import LrgMoreInfo from '../components/LrgMoreInfo';
+import Heart from '../components/Heart';
 import { BASE_URL, SM_POSTER_SIZE } from '../globals/variables.js';
 
 
@@ -12,8 +12,8 @@ function TopMovie ({ topMovieData }) {
         <div className={styles.topMovie}>
             <div className={styles.topMoviePoster}>
                 <RatingCircle rating={topMovieData[0].vote_average} className={styles.topMovieRating} />
-                <LrgMoreInfo movie={topMovieData[0]}/>
                 <img src={BASE_URL+SM_POSTER_SIZE+topMovieData[0].poster_path} alt={topMovieData.title} />
+                <Heart movie={topMovieData[0]}/>
             </div>
 
             <h3>{topMovieData[0].title}</h3>
