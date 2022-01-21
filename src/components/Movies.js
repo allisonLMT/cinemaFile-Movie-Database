9c72dc81723c2bb73083b442d13727906112841f
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-01-20 17:09:38 -0800

    moved to oneMovie

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import OneMovie from '../components/OneMovie.js';
 import styles from '../styles/movies.module.css';
-import ratingStyles from '../styles/movieRatingCircle.module.css';
-import noPoster from '../images/no-movie-poster.jpg';
-import RatingCircle from '../components/MovieRatingCircle.js';
-import Heart from '../components/Heart.js';
-import { BASE_URL, SM_POSTER_SIZE } from '../globals/variables.js';
 
 
-// movie poster, rating at top right
-//under poster, title, release date and more info icon (link which gives a pop up with the summary)
 
 
 function Movies ({ moviesData }) {
@@ -19,24 +12,7 @@ function Movies ({ moviesData }) {
         <div className={styles.movies}>
             
             {moviesData.map(movie => 
-                    <div className={styles.movie} key={movie.id}>
-                        <div className={styles.poster}>
-                            <RatingCircle rating={movie.vote_average} className={ratingStyles.circle} />
-                            <Link to={`/movie/${movie.id}`}>
-                            {movie.poster_path === null ?
-                                <img src={noPoster} alt="No Poster Available." /> :
-                                <img src={BASE_URL+SM_POSTER_SIZE+movie.poster_path} alt={movie.title} />
-                            }
-                            </Link>
-                        </div>
-                        <div className={styles.belowPoster}>
-                            <h3>{movie.title}</h3>
-                            <div className={styles.dateHeart}>
-                                <p className={styles.date}>{movie.release_date}</p>
-                                <Heart movie={movie} />  
-                            </div>
-                        </div>
-                    </div>
+                    <OneMovie movie={movie}/>
                 )} 
                 {/* end of map */}
 
