e979903c4d0cf969945bf6d810a8f29478cf3427
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-01-20 17:01:20 -0800

    right before movie card

--- src/components/TopMovie.js
+++ src/components/TopMovie.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from '../styles/topMovie.module.css';
 import RatingCircle from '../components/MovieRatingCircle.js';
 import Heart from '../components/Heart';
@@ -8,14 +8,14 @@ import { BASE_URL, SM_POSTER_SIZE } from '../globals/variables.js';
 
 function TopMovie ({ topMovieData }) {
    
-    const [isTopMovie, setIsTopMovie] = useState(true)
+   
 
     return (
         <div className={styles.topMovie}>
             <div className={styles.topMoviePoster}>
-                <RatingCircle rating={topMovieData[0].vote_average} isTopMovie={isTopMovie} />
+                <RatingCircle rating={topMovieData[0].vote_average} isTopMovie={true} />
                 <img src={BASE_URL+SM_POSTER_SIZE+topMovieData[0].poster_path} alt={topMovieData.title} />
-                <Heart movie={topMovieData[0]} isTopMovie={isTopMovie} />
+                <Heart movie={topMovieData[0]} isTopMovie={true} />
             </div>
 
             <h3>{topMovieData[0].title}</h3>
