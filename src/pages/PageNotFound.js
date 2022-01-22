29c5e57eb4531471e375cf97bd9cd476549f507e
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-01-22 11:44:56 -0800

    added Page Not Found

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function PageNotFound () {
+    console.log("not found")
+    return (
+        <div className='page'>
+            <h2>404 Error</h2>
+            <h3>Page not found.</h3>
+        </div>
+    )
+};
+
+export default PageNotFound;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import DesktopNav from '../components/DesktopNav';
 import MobileNav from '../components/MobileNav';
 import PageHome from '../pages/PageHome';
 import PageAbout from '../pages/PageAbout';
 import PageFavorites from '../pages/PageFavorites';
 import PageSingleMovie from '../pages/PageSingleMovie';
+import PageNotFound from '../pages/PageNotFound';
 
 function AppRouter() {
   return (
@@ -17,10 +18,10 @@ function AppRouter() {
         <Route path='/sort/top-rated' element={<PageHome sort='top_rated'/>} />
         <Route path='/sort/now-playing' element={<PageHome sort='now_playing'/>} />
         <Route path='/sort/upcoming' element={<PageHome sort='upcoming'/>} />
-        <Route path= "/movie/:id" element={<PageSingleMovie /> } />
-        <Route path="favorites" element={<PageFavorites />} />
-        <Route path="about" element={<PageAbout />} />
-        {/* add a Page Not Found here */}
+        <Route path='/movie/:id' element={<PageSingleMovie />} />
+        <Route path='favorites' element={<PageFavorites />} />
+        <Route path='about' element={<PageAbout />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
       <MobileNav />
     </div>
