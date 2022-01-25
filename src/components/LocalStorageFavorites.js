ef5a271e2786708f34e31f42e060fad16e8af32e
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-01-24 16:28:15 -0800

    cleaned up console.logs

--- src/components/LocalStorageFavorites.js
+++ src/components/LocalStorageFavorites.js
@@ -26,20 +26,13 @@ function addFavorite (movie) {
 
     var movieIndex = indexInFavorites(movie);
     
-    console.log(movieIndex)
-    
     //push the movie into the array of favorite movies, if it's not already there
     if (movieIndex === -1) {
         favMovies.push(movie);
-        console.log('movie added')
-        //console.log(favMovies)
-    } else {
-        console.log("already in faves")
-    }
+    } 
     
     //stringify the array and add it to local storage
     localStorage.setItem( 'favMovies' , JSON.stringify(favMovies) );
-    console.log(JSON.parse(localStorage.getItem('favMovies')));
 }
 
 
@@ -51,10 +44,9 @@ function removeFavorite (movie) {
     var movieIndex = (JSON.parse(localStorage.getItem('favMovies'))).findIndex(object => {
         return object.id === movie.id;
     });
-    console.log("movie index="+movieIndex)
+  
     // turns the localStorage back into an array before manipulating it
     var storedFaves = JSON.parse(localStorage.getItem('favMovies'));
-    console.log(JSON.parse(localStorage.getItem('favMovies')))
 
     //then remove it from the local storage
     storedFaves.splice(movieIndex, 1);
@@ -62,12 +54,10 @@ function removeFavorite (movie) {
     
     //stringify the modified array, and put it back into local storage
     localStorage.setItem( 'favMovies' , JSON.stringify(favMovies) );
-    console.log(JSON.parse(localStorage.getItem('favMovies')))
 
 }
 
 function getFavorites() {
-    console.log("getting favorites")
     var storedFaves = JSON.parse(localStorage.getItem('favMovies'));
     return storedFaves;
 }
