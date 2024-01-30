a2d310aca344800f4f0a46855b82620deda3f5c8
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2024-01-30 15:59:26 -0800

    Update MovieRatingCircle.js
    -Issue: percentages were showing (many) decimal places which didn't previously exist. (Data from the API may have changed)
    -added .toFixed to remove numbers after the decimal

--- src/components/MovieRatingCircle.js
+++ src/components/MovieRatingCircle.js
@@ -3,7 +3,7 @@ import styles from '../styles/movieRatingCircle.module.css';
 
 function MovieRatingCircle ({rating, isTopMovie}) {
     
-    const percent = rating * 10;
+    const percent = (rating * 10).toFixed();
     
     return (
         <div className={isTopMovie ? styles.topRatingCircle : styles.circle}>
