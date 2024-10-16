5ac76e1d76a516607d8a6fa4f87a68d86c346a26
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2024-10-15 17:16:23 -0700

    unignore build folder

---
+++ build/precache-manifest.4ccfcc6a1002ad25033799f1a6c2dbf8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "08185401574ca230dd23",
+    "url": "/cinemafile/static/css/main.b69f8e0d.chunk.css"
+  },
+  {
+    "revision": "08185401574ca230dd23",
+    "url": "/cinemafile/static/js/main.a22d6ae8.chunk.js"
+  },
+  {
+    "revision": "04539c8ac82cd463489f",
+    "url": "/cinemafile/static/js/runtime~main.dd1a5326.js"
+  },
+  {
+    "revision": "f15c0700e131de9f673f",
+    "url": "/cinemafile/static/js/2.2458fe7d.chunk.js"
+  },
+  {
+    "revision": "fd3a92e7dd51fe18fee8bfc0b2acced4",
+    "url": "/cinemafile/static/media/orange-cinemaFile.fd3a92e7.svg"
+  },
+  {
+    "revision": "f769def05e175799cf70ea48c4bd4a9e",
+    "url": "/cinemafile/static/media/homepage-blue.f769def0.svg"
+  },
+  {
+    "revision": "081030c610321b5b2a12e965723ab6cf",
+    "url": "/cinemafile/static/media/heart-thin-blue.081030c6.svg"
+  },
+  {
+    "revision": "9444936fb9cadd69077653059c659898",
+    "url": "/cinemafile/static/media/about-me-blue.9444936f.svg"
+  },
+  {
+    "revision": "6ee2a64a5e12115e8780f0c874481d39",
+    "url": "/cinemafile/static/media/heartAdd.6ee2a64a.svg"
+  },
+  {
+    "revision": "84448edc8c251f296380c018b059b362",
+    "url": "/cinemafile/static/media/heartFilled.84448edc.svg"
+  },
+  {
+    "revision": "24a6fdb93a3bb0275107bfc71637cb21",
+    "url": "/cinemafile/static/media/the-movie-database.24a6fdb9.svg"
+  },
+  {
+    "revision": "7239b1e4195337a272dd8122bedd5057",
+    "url": "/cinemafile/index.html"
+  }
+];
